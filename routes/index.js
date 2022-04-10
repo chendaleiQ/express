a66c3196cd45b35bee5c55ae729b7c0db9cc259f
chendaleiQ <chenzhongqing0724@gmail.com>
2022-04-10 12:19:34 +0800

    sssss

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../config/db');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+router.get('/getGirlFriend', function(req, res, next) {
+  console.log(req.body,req.params)
+  let obj = {
+    name:'李玉华',
+    age:'23',
+    weight:'99'
+  }
+  res.send(obj);
+});
+module.exports = router;
