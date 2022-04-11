1bd6aae6d4ed5c1aa15ee8191835b2833a583265
chendaleiQ <chenzhongqing0724@gmail.com>
2022-04-11 21:47:14 +0800

    change:主页

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 var db = require('../config/db');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'index' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'index'});
 });
-router.get('/getGirlFriend', function(req, res, next) {
-  let obj = {
-    name:'李玉华',
-    age:'23',
-    weight:'99'
-  }
-  res.send(obj);
+router.get('/getNum', (req, res) => {
+    res.send(Math.ceil(Math.random() * 100))
+})
+router.get('/getGirlFriend', function (req, res, next) {
+    let obj = {
+        name: '李玉华',
+        age: '23',
+        weight: '99'
+    }
+    res.send(obj);
 });
 module.exports = router;
