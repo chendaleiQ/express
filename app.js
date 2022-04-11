b7a84c2ed8f66a618e8d160c42104d386d5a92e3
chendaleiQ <chenzhongqing0724@gmail.com>
2022-04-11 22:11:17 +0800

    change:主页

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-// var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,27 @@ var router = express.Router();
 var db = require('../config/db');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'index'});
+    // req 是一个可读流
+    req.on('data', chunk => {
+        console.log(chunk)
+    })
+    req.on('end', () => {})
+    // 响应行->响应头->响应体顺序不能变
+    // 首先设置响应行（状态码与状态码描述）
+    res.statusCode = 200
+    res.statusMessage = 'success'
+    // 设置响应头
+    res.setHeader('name', 'superYue')
+    // 最后设置响应体
+    // res 是一个可写流
+    res.write('ok')
+    res.end('1')
+    // res.render('index', {title: 'index'});
 });
 router.get('/getNum', function (req, res) {
-    res.send(Math.ceil(Math.random() * 100))
+    res.send({
+        '您的幸运数字': Math.ceil(Math.random() * 100)
+    })
 })
 router.get('/getGirlFriend', function (req, res, next) {
     let obj = {
